0cbed612bd146c5dfb4db3ba1445d62e905163fc
upwon <31719953+upwon@users.noreply.github.com>
2018-10-26 17:44:38 +0800

    Add files via upload
    opencv 播放视频

---
+++ 播放视频/PlayVideo/main.cpp
@@ -0,0 +1,30 @@
+#include<opencv2/opencv.hpp>
+
+using namespace cv;
+
+int main()
+{
+	//1. 读入视频
+	VideoCapture capture("../1.avi");
+
+	//2.循环显示每一帧
+	while (1)
+	{
+		Mat frame;			      //定义一个Mat变量，用于存储每一帧图像
+		capture >> frame;	      //读取当前帧
+
+		//若视频读取完，退出循环
+		if (frame.empty())
+		{
+			break;
+		}
+
+		imshow("读取视频",frame); //显示当前帧
+
+		waitKey(30);  //延时30mS
+
+	}
+
+
+	return 0;
+}
